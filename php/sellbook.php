4ad294aa5551a38141cf147b9e93faec8d833099
laronandrew11 <laronandrew11@yahoo.com>
2014-02-17 20:28:14 +0800

    Added session/password validation; fixed null value being received when "Negotiable" was unchecked

--- php/sellbook.php
+++ php/sellbook.php
@@ -27,23 +27,27 @@ if(isset($_POST['submit'])){
 	if(isset($_POST['negotiable'])) {
 	$negotiable = mysql_real_escape_string(true);
 	}
+	else {$negotiable=mysql_real_escape_string(0);}
 	$meetup = mysql_real_escape_string($_POST['meetup_place']);
 	$password = mysql_real_escape_string($_POST['user_password']); //hashing to be added in future
 	
+	session_start(); 
 	$seller =  $_SESSION['username']; //not sure if this is right
 	
 	$add_book ="INSERT INTO Book (title, authors, publisher, isbn, category, subjects)
         VALUES ('$title','$authors','$publisher',$isbn,'$category','$subjects')"; //later on, add a way to save and reference categories and subjects
 	
-
+	$seller_row=mysql_fetch_array(mysql_query("SELECT * FROM UserAccount WHERE username = '$seller' AND 
+        passwordhash='$password'  LIMIT 1"));
+				$sellerid=mysql_real_escape_string((int)$seller_row['id']);
+		if($sellerid!=0)
+		{
 		if(mysql_query($add_book)){
 			$get_book_id=mysql_query("SELECT * FROM Book WHERE title='$title'");
 			if(mysql_num_rows($get_book_id) == 1){
 				$row= mysql_fetch_array($get_book_id); 
 				$bookid= mysql_real_escape_string((int)$row['id']);
-				$seller_row=mysql_fetch_array(mysql_query("SELECT * FROM UserAccount WHERE username = '$seller' AND 
-        passwordhash='$password'  LIMIT 1"));
-				$sellerid=
+				
 				/*echo $bookid;
 				echo $price;
 				echo $negotiable;
@@ -58,6 +62,8 @@ if(isset($_POST['submit'])){
 			else{echo "failed to query book id";}
 		}
 		else {echo "failed to add book";}
+		}
+		else echo "Invalid user or password";
 		
 					
 			
