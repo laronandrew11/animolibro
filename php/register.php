58424e494eb2097b3683f5502ca9ec7a76186303
laronandrew11 <laronandrew11@yahoo.com>
2014-02-25 10:06:32 +0800

    Now actually WORKS

--- php/register.php
+++ php/register.php
@@ -15,46 +15,27 @@ if(isset($_POST['submit'])){
     The Above code can be in a different file, then you can place include'filename.php'; instead. 
     */ 
      
-	$name = mysql_real_escape_string($_POST['full_name']);
+	$name = mysql_real_escape_string($_POST['user_name']);
 	$email = mysql_real_escape_string($_POST['user_email']);
-	$contact = mysql_real_escape_string($_POST['contact_number']);
+	$contact = mysql_real_escape_string($_POST['user_contactno']);
 	$course = mysql_real_escape_string($_POST['course']);
-	$password = mysql_real_escape_string($_POST['password']);
-	$re_password = mysql_real_escape_string($_POST['re_password']);
-	
-	$sql = mysql_query($query); 
-    $row = mysql_fetch_array($sql);
-	
-	if($row||empty($_POST['full_name'])|| empty($_POST['user_email'])||empty($_POST['contact_number'])||empty($_POST['password'])|| empty($_POST['re_password'])||$_POST['password']!=$_POST['re_password']){ 
-        // if a field is empty, or the passwords don't match make a message 
-		 $error = '<p>'; 
-        if(empty($_POST['full_name'])){ 
-            $error .= 'Name can\'t be empty<br>'; 
-        } 
-        if(empty($_POST['user_email'])){ 
-            $error .= 'Email can\'t be empty<br>'; 
-        } 
-        if(empty($_POST['contact_number'])){ 
-            $error .= 'Contact Number can\'t be empty<br>'; 
-        } 
-        if(empty($_POST['password'])){ 
-            $error .= 'Password can\'t be empty<br>'; 
-        } 
-        if(empty($_POST['re_password'])){ 
-            $error .= 'Please re-type password<br>'; 
-        } 
-        if($_POST['password']!=$_POST['re_password']){ 
-            $error .= 'Passwords don\'t match<br>'; 
-        } 
-        $error .= '</p>' 
-	 }else{ 
-        // If all fields are not empty, and the passwords match, 
-        // create a session, and session variables, 
-        $query = sprintf("INSERT INTO UserAccount(`username`,`email`,`contactnumber`,`Course_id`,`passwordhash`) 
-            VALUES('$name','$email','$contact','$course','$password')";
-        $sql = mysql_query($query);  
-        exit; 
-    } 
+	$password = mysql_real_escape_string($_POST['user_password']);
+	//$re_password = mysql_real_escape_string($_POST['confirm_password']);
+
+
+        $query = "INSERT INTO UserAccount(username,email,contactnumber,Course_id,passwordhash) 
+            VALUES('$name','$email',$contact,$course,$password)";
+	echo $query;
+    if(mysql_query($query))
+	{
+	 session_start(); 
+        $_SESSION['animolibrousername'] = $row['username'];
+		$_SESSION['logged'] = true;
+        //header("Location: http://localhost/animolibro/php/users_page.php"); 
+		header("Location: http://localhost/animolibro/home.php");
+	}else{ echo "Registration failed";}
+    exit; 
 } 	
+else {echo "Not submitted!";}
 
 ?> 
