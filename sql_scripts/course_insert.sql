c187f7ba8d803db889acf7b70bec3678267ecdab
laronandrew11 <laronandrew11@yahoo.com>
2014-02-25 10:23:00 +0800

    Course Insert script, to ensure that registration will work

---
+++ sql_scripts/course_insert.sql
@@ -0,0 +1,1 @@
+INSERT INTO Course (name, code) VALUES ('Software Technology', 'CS-ST'),('Computer Systems Engineering','CS-CSE'),('Network Engineering','CS-NE'),('Instructional Systems Technology','CS-IST');
