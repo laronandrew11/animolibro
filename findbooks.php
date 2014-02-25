6b4c803ff2996efa463dd3f7ada1efd3eff5ba7f
laronandrew11 <laronandrew11@yahoo.com>
2014-02-25 11:18:55 +0800

    Incorporated back-end of search

--- findbooks.php
+++ findbooks.php
@@ -51,13 +51,165 @@
         </div><!--/.nav-collapse -->
       </div>
     </div>';
-	
-?>
-	<div class="container">
+	echo '<div class="container">
 	<div class="row">
 	<div class="col-sm-8 col-md-8">
 		<div class="col-lg-6">
-        
+        <form action="findbooks.php" class="form-horizontal" role="form" id="searchForm" method="post" >
+			<legend>Find a Book</legend>
+		
+			<div class="form-group">
+			<label class="control-label">Search by Title:</label>
+			<div class="controls">
+			<input type="text" class="input-xlarge form-control" id="book_title" name="book_title" rel="popover" data-content="Enter the book full title." data-original-title="Title">
+			</div>
+			</div>
+			
+			<div class="form-group">
+			<label class="control-label">Search by ISBN:</label>
+			<div class="controls">
+			<input type="text" class="input-xlarge form-control" id="book_isbn" name="book_isbn" rel="popover" data-content="Enter the book ISBN." data-original-title="ISBN">
+			</div>
+			</div>
+			
+			<div class="form-group">
+			<label class="control-label">Search by Author:</label>
+			<div class="controls">
+			<input type="text" class="input-xlarge form-control" id="book_author" name="book_author" rel="popover" data-content="Enter the book author." data-original-title="Author">
+			</div>
+			</div>
+			
+			<div class="form-group">
+			<label class="control-label">Subject:</label> 
+			<div class="controls">
+			<select name="subject">
+			<option value="ALGOCOM">ALGOCOM</option>
+			<option value="HCIFACE">HCIFACE</option>
+			<option value="PROFSWD">PROFSWD</option>
+			<option value="STRESME">STRESME</option>
+			<option value="WEBAPPS">WEBAPPS</option>
+			</select>
+			</div>
+			</div>
+			
+			<div class="form-group">
+			<label class="control-label">Category:</label> 
+			<div class="controls">
+			<select name="category">
+			<option value="Computer">Computer</option>
+			<option value="Law">Law</option>
+			<option value="Mathematics">Mathematics</option>
+			<option value="Science">Science</option>
+			</select>
+			</div>
+			</div>
+	
+			<div class="form-group">
+			<label class="control-label">Publisher:</label>
+			<div class="controls">
+			<input type="text" class="input-xlarge form-control" id="book_publisher" name="book_publisher" rel="popover" data-content="Enter the book publisher" data-original-title="Publisher:">
+			</div>
+			</div>
+
+			<div class="form-group">
+			<label class="control-label">Sort By</label> 
+			<div class="controls">
+			<select name="sortby">
+			<option value="choice1">Date Added</option>
+			<option value="choice2">Number of Copies</option>
+			<option value="choice3">Title</option>
+			<option value="choice4">Category</option>
+			<option value="choice5">Subject</option>
+			</select>
+			</div>
+			</div>
+			<div class="controls">
+		<input type="submit" class="btn btn-success" role="button" name = "submit" value = "Search">
+		
+		</div>
+			
+			</form>
+			<!--a role="button" href="findbooks.html" class="btn btn-success"> <i class="glyphicon glyphicon-search"></i></a-->
+    </div>
+	</div>
+    </div>
+	<div class="row">
+        <div class="col-lg-4 center">
+			<h4>Search Results</h4>';
+	
+	if(isset($_POST['submit'])){ 
+    $dbHost = "localhost";        //Location Of Database usually its localhost 
+    $dbUser = "root";            //Database User Name 
+    $dbPass = "";            //Database Password 
+    $dbDatabase = "animolibrosimple";    //Database Name 
+     
+    $db = mysql_connect($dbHost,$dbUser,$dbPass)or die("Error connecting to database."); 
+    //Connect to the databasse 
+    mysql_select_db($dbDatabase, $db)or die("Couldn't select the database."); 
+    //Selects the database 
+     
+    /* 
+    The Above code can be in a different file, then you can place include'filename.php'; instead. 
+    */ 
+
+    $title = mysql_real_escape_string($_POST['book_title']); 
+    $isbn = mysql_real_escape_string($_POST['book_isbn']); 
+	 $author = mysql_real_escape_string($_POST['book_author']); 
+	  $subject = mysql_real_escape_string($_POST['subject']); 
+	 $category = mysql_real_escape_string($_POST['category']); 
+	  $publisher = mysql_real_escape_string($_POST['book_publisher']); 
+	
+	$query ="SELECT * FROM Book  
+        WHERE (title LIKE '%$title%'
+		OR isbn = $isbn OR authors LIKE '%$author%') 
+		AND (subjects ='$subject'
+		AND category = '$category'
+		AND publisher LIKE '%$publisher%')";
+		//echo $query;
+	$sql = mysql_query($query);
+    if(mysql_num_rows($sql) >= 1){ 
+		while($row = mysql_fetch_array($sql))
+		{
+			//echo $row['title'] . " " . $row['LastName'];
+			// "<br>";
+			echo ' <div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">';
+					echo $row['title'];
+					echo'</h3>
+				</div>
+				<div class="panel-body">
+					<div class="col-sm-6 col-md-4">
+                        <img src="assets/bookcoversmall.jpg"  alt="" class="img-rounded img-responsive" />
+                    </div>
+					<p>Category: ';
+					echo $row['category'];
+					echo '<p>Author: ';
+					echo $row['authors'];
+					
+					echo'<p>Copies Available: 4
+					<a role="button" href = "bookprofile.html" class="btn btn-primary pull-right" role=>View</a>
+				</div>
+			</div>';
+		}
+		
+        exit; 
+    }else{ 
+        echo "No results found";
+        exit; 
+    } 
+
+}else{    //If the form button wasn't submitted go to the index page, or login page 
+  
+} 
+	
+	
+?>
+	<!--div class="container">
+	<div class="row">
+	<div class="col-sm-8 col-md-8">
+		<div class="col-lg-6">
+        <form action="findbooks.php" class="form-horizontal" role="form" id="searchForm" method="post" >
 			<legend>Find a Book</legend>
 		
 			<div class="form-group">
@@ -84,12 +236,12 @@
 			<div class="form-group">
 			<label class="control-label">Subject:</label> 
 			<div class="controls">
-			<select name="category">
-			<option value="choice1">ALGOCOM</option>
-			<option value="choice2">HCIFACE</option>
-			<option value="choice3">PROFSWD</option>
-			<option value="choice4">STRESME</option>
-			<option value="choice5">WEBAPPS</option>
+			<select name="subject">
+			<option value="ALGOCOM">ALGOCOM</option>
+			<option value="HCIFACE">HCIFACE</option>
+			<option value="PROFSWD">PROFSWD</option>
+			<option value="STRESME">STRESME</option>
+			<option value="WEBAPPS">WEBAPPS</option>
 			</select>
 			</div>
 			</div>
@@ -98,10 +250,10 @@
 			<label class="control-label">Category:</label> 
 			<div class="controls">
 			<select name="category">
-			<option value="choice1">Computer</option>
-			<option value="choice2">Law</option>
-			<option value="choice3">Mathematics</option>
-			<option value="choice4">Science</option>
+			<option value="Computer">Computer</option>
+			<option value="Law">Law</option>
+			<option value="Mathematics">Mathematics</option>
+			<option value="Science">Science</option>
 			</select>
 			</div>
 			</div>
@@ -116,7 +268,7 @@
 			<div class="form-group">
 			<label class="control-label">Sort By</label> 
 			<div class="controls">
-			<select name="category">
+			<select name="sortby">
 			<option value="choice1">Date Added</option>
 			<option value="choice2">Number of Copies</option>
 			<option value="choice3">Title</option>
@@ -125,15 +277,20 @@
 			</select>
 			</div>
 			</div>
-				
-			<a role="button" href="findbooks.html" class="btn btn-success"> <i class="glyphicon glyphicon-search"></i></a>
-    </div>
+			<div class="controls">
+		<input type="submit" class="btn btn-success" role="button" name = "submit" value = "Search">
+		
+		</div>
+			
+			</form>
+			<!--a role="button" href="findbooks.html" class="btn btn-success"> <i class="glyphicon glyphicon-search"></i></a-->
+    <!--/div>
 	</div>
     </div>
 	<div class="row">
         <div class="col-lg-4 center">
-			<h4>Search Results</h4>
-          <div class="panel panel-default">
+			<h4>Search Results</h4-->
+          <!--div class="panel panel-default">
 				<div class="panel-heading">
 					<h3 class="panel-title">Fundamentals of Web Design, 4th Edition</h3>
 				</div>
