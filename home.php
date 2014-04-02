1b651be47864442223ebf34516a84750ed8a134f
laronandrew11 <laronandrew11@yahoo.com>
2014-04-02 11:11:41 +0800

    Home jumbotrons now side-to-side on large screens

--- home.php
+++ home.php
@@ -8,22 +8,24 @@ include ('navbar.php');
 	
 ?>
 	<!--script src ="home.php"></script-->
-	<div class="jumbotron">
+	
+	<div class="jumbotron col-md-6 col-lg-6 ">
 	<h1>Sell a book</h1>
 		<p>Done with your old textbooks? Let them continue to serve their purpose.</p>
 		<p><a class="btn btn-primary btn-lg" role="button" href="sellbookpage.php">Sell a textbook</a></p>
 	</div>
-	<div class="jumbotron">
+	<div class=" jumbotron col-md-6 col-lg-6 ">
 	<h1>Find a book</h1>
 		<p>Need a textbook? Start your search here.</p>
-		<p><div class="row">
-        <div class="col-lg-6">
+		<p><!--div class="row"-->
+        <div class="col-lg-12">
             <form name=search action="findbooks.php" method="post"  class="form-inline" >
                 <input class="span5 form-control input-xlarge" type="text"  id="typeahead" name="typeahead" rel="popover" data-provide="typeahead" placeholder="Search by Title, ISBN, Author, Publisher, Category, or Subject:">
-                <button type="submit" name="submit" class="btn btn-success"> Search <i class="glyphicon glyphicon-search"></i></button>
+                <button type="submit" name="submit" class="btn btn-success btn-lg"> Search <i class="glyphicon glyphicon-search"></i></button>
 				 <!--a name = "submit0" type="submit" role="button" class="btn btn-success"> <i class="glyphicon glyphicon-search"></i></a-->
             </form>
         </div></p>
+	<!--/div-->
 	</div>
 
 	      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
