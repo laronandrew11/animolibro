79441f88b0c34162727be956b1e5e8d1f551ebe9
laronandrew11 <laronandrew11@yahoo.com>
2014-08-13 14:38:51 +0800

    Added authenticator file to keep anonymous users out of members-only areas

---
+++ php/authenticator.php
@@ -0,0 +1,14 @@
+<?php
+if(!($_SESSION['animolibrousername'])){
+	//reject the user
+		
+		header("Location: http://localhost/animolibro/portal.php");
+		//echo '<li><a href="userprofile.php?user='.$_SESSION["animolibrousername"].'"><span class="glyphicon glyphicon-user"></span> ';
+		}
+	else 
+	//accept the user
+	{
+		//echo '<li  class="active"><a href="userprofile.php?user='.$_SESSION["animolibrousername"].'"><span class="glyphicon glyphicon-user"></span> ';
+
+	}
+	?>
