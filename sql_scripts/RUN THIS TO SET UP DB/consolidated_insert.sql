ae2ab1851e86f987fd69fa9104c558693b211033
laronandrew11 <laronandrew11@yahoo.com>
2014-08-13 10:45:42 +0800

    added sample subjects

--- sql_scripts/RUN THIS TO SET UP DB/consolidated_insert.sql
+++ sql_scripts/RUN THIS TO SET UP DB/consolidated_insert.sql
@@ -1,1 +1,3 @@
 INSERT INTO Course (name, code) VALUES ('Software Technology', 'CS-ST'),('Computer Systems Engineering','CS-CSE'),('Network Engineering','CS-NE'),('Instructional Systems Technology','CS-IST');
+
+INSERT INTO Subject (code) VALUES ('HCIFACE'),('SECUDEV'),('HUMAART'),('WEBAPPS'),('COMPRO1'),('ACTBAS');
