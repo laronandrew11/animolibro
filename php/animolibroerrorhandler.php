683acf6d4d4556c9ba5e7fd505aa184eb74a2e0e
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2014-08-17 13:27:34 +0800

    Removed echo in error handler

--- php/animolibroerrorhandler.php
+++ php/animolibroerrorhandler.php
@@ -1,7 +1,6 @@
 <?php
 
 function animo_error_handler($errno, $errstr, $errfile, $errline) {
-    echo "'$errno', '$errstr', '$errfile', '$errline'<br />";
 
     $errno = mysql_escape_string($errno);
     $errstr = mysql_escape_string($errstr);
@@ -13,7 +12,6 @@ function animo_error_handler($errno, $errstr, $errfile, $errline) {
     include('dbConnect.php');
     if (mysql_query($error_query)) {
 		/* Successfully logged into db */
-		echo "Error inserted.";
     }
     else {
     	/* Failed to log into db */
