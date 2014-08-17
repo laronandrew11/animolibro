0e5c5783ac03ddb6ed1a2fc818af75593662672d
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2014-08-17 13:26:27 +0800

    Working error logger

--- php/animolibroerrorhandler.php
+++ php/animolibroerrorhandler.php
@@ -3,6 +3,22 @@
 function animo_error_handler($errno, $errstr, $errfile, $errline) {
     echo "'$errno', '$errstr', '$errfile', '$errline'<br />";
 
+    $errno = mysql_escape_string($errno);
+    $errstr = mysql_escape_string($errstr);
+    $errfile = mysql_escape_string($errfile);
+    $errline = mysql_escape_string($errline);
+
+    $error_query = "INSERT INTO `log_errors` (`errno`, `errstr`, `errfile`, `errline`) VALUES ('$errno', '$errstr', '$errfile', '$errline')";
+
+    include('dbConnect.php');
+    if (mysql_query($error_query)) {
+		/* Successfully logged into db */
+		echo "Error inserted.";
+    }
+    else {
+    	/* Failed to log into db */
+    }
+
     return true;
 }
 
