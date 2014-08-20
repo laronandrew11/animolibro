2b9273a7fdf2658bd0222fad974a471e1f540046
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2014-08-20 11:15:43 +0800

    Working confirmstat.php; Can now accept/reject requests to buy

--- php/confirmstat.php
+++ php/confirmstat.php
@@ -1,54 +1,38 @@
 <?php 
 include_once('animolibroerrorhandler.php');
 require_once("db_config.php");
-if(isset($_POST['submit1'])&&$_POST['myprofile']==true){ 
-   include('dbConnect.php');
-   $db = database::getInstance(); 
-     
-    $adid = mysql_real_escape_string($_POST['adid']);  //change to number instead of string?
-	$status = mysql_real_escape_string($_POST['submit1']);
-	session_start(); 
-	$buyerid =  $_SESSION['animolibroid']; //not sure if this is right
-	if($status == "Accept")
-	{
-		$stmt = $db->dbh->prepare("UPDATE Ad SET status = :status WHERE id = :adid");
+if (isset($_POST['submit1']) && $_POST['myprofile'] == true) {
+	$db = database::getInstance();
 	
-		if($stmt->execute(array(':status' => 2,':id' => $_POST['adid']))){
-		header("Location: http://localhost/animolibro/userprofile.php?user=".$_SESSION['animolibrousername']);
-		}
-		else header("Location: http://localhost/errorpage.php"); 
-	        //session_start(); 
-	        //$_SESSION['username'] = $row['username'];
-	        //header("Location: http://localhost/animolibro/home.html"); // Modify to go to the page you would like 
-	    exit; 
-	//	$update_ad ="UPDATE Ad SET status = 2 WHERE id = $adid"; 
+	$adid = $_POST['adid'];
+	$status = $_POST['submit1'];
+	
+	session_start();
+	$buyerid = $_SESSION['animolibroid']; //not sure if this is right
+	
+	if ($status == "Accept") {
+		$status_id = 2;
 	}
-	else
-	{
-		$stmt = $db->dbh->prepare("UPDATE Ad SET status = :status WHERE id = :adid");
-		
-		if($stmt->execute(array(':status' => 3,':id' => $_POST['adid']))){
-		header("Location: http://localhost/animolibro/userprofile.php?user=".$_SESSION['animolibrousername']);
-		}
-		else header("Location: http://localhost/errorpage.php"); 
-	        //session_start(); 
-	        //$_SESSION['username'] = $row['username'];
-	        //header("Location: http://localhost/animolibro/home.html"); // Modify to go to the page you would like 
-	    exit; 
-	//	$update_ad ="UPDATE Ad SET status = 3 WHERE id = $adid";
+	else if ($status == "Reject") {
+		$status_id = 3;
 	}
-	/*
-	if(mysql_query($update_ad)){
-		header("Location: http://localhost/animolibro/userprofile.php?user=".$_SESSION['animolibrousername']);
-	}
-	else header("Location: http://localhost/errorpage.php"); 
-        //session_start(); 
-        //$_SESSION['username'] = $row['username'];
-        //header("Location: http://localhost/animolibro/home.html"); // Modify to go to the page you would like 
-    exit; */
+	
+	$stmt = $db->dbh->prepare("UPDATE Ad SET status = :status WHERE id = :adid");
+	$stmt->bindParam(':adid', $adid);
+	$stmt->bindParam(':status', $status_id);
 
-}else{    //If the form button wasn't submitted go to the index page, or login page 
-	header("Location: http://localhost/animolibro/home.php");     
-    exit; 
+	if ($stmt->execute()) {
+		header("Location: ../userprofile.php?user=".$_SESSION['animolibrousername']);
+	}
+	else {
+		header("Location: ../errorpage.php");
+	}
+	exit;
+}
+
+else{
+	//If the form button wasn't submitted go to the index page, or login page 
+	header("Location: ../home.php");	 
+	exit;
 }
 ?>
