8f225081760a0554e452e55f13c97e296b0b15a4
laronandrew11 <laronandrew11@yahoo.com>
2014-08-20 17:15:30 +0800

    revert

--- registerpage.php
+++ registerpage.php
@@ -68,7 +68,16 @@ include('navbar_out.php');
 		<div class="controls">
 		<select name="course">
 <?php
-include('coursefetcher.php');
+$db = database::getInstance();
+$course_query = $db->dbh->prepare("SELECT * FROM course");
+
+if($course_query->execute()) { 
+	while($course_row = $course_query->fetch(PDO::FETCH_ASSOC)) {
+		$course_id = $course_row['id'];
+		$course_code = $course_row['code'];
+		echo '<option value='.$course_id.'>'.$course_code.'</option>';
+	}
+}
 ?>
 		</select>
 		</div>
