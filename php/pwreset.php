31045fc01284cf902212e45cd3b9514d0ec47f2a
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2014-08-21 12:06:53 +0800

    PDO-fied Password Reset and made it reflect new password hash

--- php/pwreset.php
+++ php/pwreset.php
@@ -2,29 +2,52 @@
 include_once('animolibroerrorhandler.php');
 require_once("db_config.php");
 
-if(isset($_POST['submit'])){ 
-   include('dbConnect.php');
+if (isset($_POST['submit'])) {
 	$db = database::getInstance(); 
 
-   $email = mysql_real_escape_string($_POST['email']);
-   $hash = mysql_real_escape_string($_POST['hash']);
-   $newpass = mysql_real_escape_string($_POST['newpass']);
-   $newpassagain = mysql_real_escape_string($_POST['newpassagain']);
-   if($newpass == $newpassagain){
-		$pwhash = md5($newpass);
-	   
-		$stmt = $db->dbh->prepare("UPDATE UserAccount SET passwordhash=:passwordhash WHERE email=:email AND passwordhash=:hash");
-	   
-	    if($stmt->execute(array(':passwordhash' => $pwhash, ':email' => $_POST['email'], ':passwordhash' => $_POST['hash']))) {
-			$_SESSION['pwchange']=true;
-			header("Location: http://localhost/animolibro/portal.php");
-		}else { 
-			$_SESSION['pwchange']=false;
-			header("Location: http://localhost/animolibro/portal.php");
+	$email = $_POST['email'];
+	$hash = $_POST['hash'];
+	$newpass = $_POST['newpass'];
+	$newpassagain = $_POST['newpassagain'];
+
+	$password_query = $db->dbh->prepare("SELECT passwordhash, salt FROM UserAccount WHERE email = :email");
+	$password_query->bindParam(':email', $email);
+	if ($password_query->execute()) {
+		$password_row = $password_query->fetch(PDO::FETCH_ASSOC);
+		$stored_password_hash = $password_row['passwordhash'];
+		$stored_salt = $password_row['salt'];
+
+		if ($hash == $stored_password_hash) {
+			if ($newpass == $newpassagain) {
+				$newpass_hash = hash("sha256", $newpass.$stored_salt);
+					
+				$password_update_query = $db->dbh->prepare("UPDATE UserAccount SET passwordhash = :passwordhash WHERE username = :username");
+				$password_update_query->bindParam(':passwordhash', $newpass_hash);
+				$password_update_query->bindParam(':username', $username);
+					
+				if($password_update_query->execute()) {
+					$_SESSION['pwchange'] = true;
+					header("Location: ../home.php");
+					exit;
+				}
+				else {
+					$_SESSION['pw_db_failed'] = true;
+				}
+			}
+			else {
+				$_SESSION['pw_not_equal'] = true;
+			}
 		}
-		exit; 
+		else {
+			$_SESSION['pw_wrong'] = true;
+		}
 	}
-	else{echo "Change Password failed";}
-}   
+	else {
+		$_SESSION['pw_db_failed'] = true;
+	}
+	$_SESSION['pwchange'] = false;
+	header("Location: ../resetpassword.php?email=".$email."&hash=".$hash);
+	exit;
+}	
 else {echo "Not submitted!";}
 ?>
